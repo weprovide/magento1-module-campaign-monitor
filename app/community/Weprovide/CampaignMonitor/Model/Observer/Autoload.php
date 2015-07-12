0daf1469984c05fb8ee3ef78af34b4246e04e135
Lex Beelen <lex@lexperts.nl>
2015-07-12 20:35:27 +0200

    Move observer files to separate directory

--- app/community/Weprovide/CampaignMonitor/Model/Observer/Autoload.php
+++ app/community/Weprovide/CampaignMonitor/Model/Observer/Autoload.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Class Weprovide_CampaignMonitor_Model_Autoload
+ * Class Weprovide_CampaignMonitor_Model_Observer_Autoload
  *
  * @author Lex Beelen <lex@weprovide.com>
  * @copyright Copyright (c) 2015, We/Provide http://www.weprovide.com
  */
-class Weprovide_CampaignMonitor_Model_Autoload
+class Weprovide_CampaignMonitor_Model_Observer_Autoload
 {
     /**
      * controllerFrontInitBefore
@@ -24,6 +24,7 @@ class Weprovide_CampaignMonitor_Model_Autoload
         $campaignmonitorLibrary = Mage::getBaseDir('lib') . '/CampaignMonitor/';
         require_once($campaignmonitorLibrary . 'csrest_subscribers.php');
         require_once($campaignmonitorLibrary . 'csrest_clients.php');
+        require_once($campaignmonitorLibrary . 'csrest_lists.php');
     }
 
 }
--- app/community/Weprovide/CampaignMonitor/Model/Observer/Subscribe.php
+++ app/community/Weprovide/CampaignMonitor/Model/Observer/Subscribe.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Class Weprovide_CampaignMonitor_Model_Observer
+ * Class Weprovide_CampaignMonitor_Model_Observer_Subscribe
  *
  * @author Lex Beelen <lex@weprovide.com>
  * @copyright Copyright (c) 2015, We/Provide http://www.weprovide.com
  */
-class Weprovide_CampaignMonitor_Model_Observer
+class Weprovide_CampaignMonitor_Model_Observer_Subscribe
 {
     /**
      * @param Varien_Event_Observer $observer
@@ -21,14 +21,14 @@ class Weprovide_CampaignMonitor_Model_Observer
             switch ($subscriber->getSubscriberStatus()) {
                 case 1:
                     try {
-                        Mage::getModel('campaignmonitor/api')->subscribe($subscriber->getSubscriberEmail(), $subscriber->getSubscriberId(), $subscriber->getSubscriberConfirmCode(), $subscriber->getCustomerId(), $subscriber->getStoreId());
+                        Mage::getModel('campaignmonitor/api_subscribers')->subscribe($subscriber->getSubscriberEmail(), $subscriber->getSubscriberId(), $subscriber->getSubscriberConfirmCode(), $subscriber->getCustomerId(), $subscriber->getStoreId());
                     } catch (Exception $e) {
                         Mage::log($e->getMessage(), NULL, 'campaignmonitor.log');
                     }
                     break;
                 case 3:
                     try {
-                        Mage::getModel('campaignmonitor/api')->unsubscribe($subscriber->getSubscriberEmail(), $subscriber->getStoreId());
+                        Mage::getModel('campaignmonitor/api_subscribers')->unsubscribe($subscriber->getSubscriberEmail(), $subscriber->getStoreId());
                     } catch (Exception $e) {
                         Mage::log($e->getMessage(), NULL, 'campaignmonitor.log');
                     }
@@ -48,7 +48,7 @@ class Weprovide_CampaignMonitor_Model_Observer
         if(Mage::getModel('campaignmonitor/setting')->isEnabled($subscriber->getStoreId()))
         {
             try {
-                Mage::getModel('campaignmonitor/api')->delete($subscriber->getSubscriberEmail(), $subscriber->getStoreId());
+                Mage::getModel('campaignmonitor/api_subscribers')->delete($subscriber->getSubscriberEmail(), $subscriber->getStoreId());
             } catch (Exception $e) {
                 Mage::log($e->getMessage(), NULL, 'campaignmonitor.log');
             }
