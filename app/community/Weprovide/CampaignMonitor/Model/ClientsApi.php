911254799f6193e4a2461055603678ccc43bb2e0
Tim Neutkens <tim@weprovide.nl>
2015-07-12 18:29:15 +0200

    Load clients api through Autoload.php
    Load campaign monitor clients api through Autoload.php

--- app/community/Weprovide/CampaignMonitor/Model/ClientsApi.php
+++ app/community/Weprovide/CampaignMonitor/Model/ClientsApi.php
@@ -1,11 +1,6 @@
 <?php
 
 /**
- * Load subscriber CampaignMonitor Library
- */
-require_once(Mage::getBaseDir('lib') . '/CampaignMonitor/csrest_clients.php');
-
-/**
  * Class Weprovide_CampaignMonitor_Model_ClientsApi
  *
  * @author Tim Neutkens <tim@weprovide.com>
